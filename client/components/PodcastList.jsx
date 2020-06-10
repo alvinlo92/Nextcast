f17ef7963649dbf0926b9df46e752cb1089f4436
alvinlo92 <alvinlo92@yahoo.com>
2020-06-09 17:33:38 -0700

    refactor: change all components to use react-redux hooks

--- client/components/PodcastList.jsx
+++ client/components/PodcastList.jsx
@@ -1,44 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class PodcastList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showPlayer: false,
-    };
-
-    this.handleShowPlayer = this.handleShowPlayer.bind(this);
-  }
-
-  handleShowPlayer() {
-    const { showPlayer } = this.state;
-    if (showPlayer) {
-      this.setState({ showPlayer: false });
-    } else {
-      this.setState({ showPlayer: true });
-    }
-  }
-
-  render() {
-    const { showPlayer } = this.state;
-    const { episode } = this.props;
-    return (
-      <div>
-        <div>Title</div>
-        <div>{episode.title}</div>
-        <div>{episode.description[0]}</div>
-        {showPlayer ? <button type="button" onClick={this.handleShowPlayer}>Stop Playing</button> : <button type="button" onClick={this.handleShowPlayer}>Play Episode</button>}
-        {showPlayer && (
-          <div>
-            <video controls>
-              <source src={episode.enclosure.url} />
-            </video>
-          </div>
-        )}
-        <br />
-      </div>
-    );
-  }
-}
+const PodcastList = () => {
+  return (
+    <div>
+      <div>Title</div>
+      <div>{episode.title}</div>
+      <div>{episode.description[0]}</div>
+      {showPlayer ? <button type="button" onClick={this.handleShowPlayer}>Stop Playing</button> : <button type="button" onClick={this.handleShowPlayer}>Play Episode</button>}
+      {showPlayer && (
+        <div>
+          <video controls>
+            <source src={episode.enclosure.url} />
+          </video>
+        </div>
+      )}
+      <br />
+    </div>
+  );
+};
 
 export default PodcastList;
